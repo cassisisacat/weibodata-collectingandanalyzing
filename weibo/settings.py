2878c28575b55d2f4402d63b2c8b3dec5a7bb724
Cassis Martin <71492383+cassisisacat@users.noreply.github.com>
2022-03-19 23:21:34 +0800

    Add files via upload

--- weibo/settings.py
+++ weibo/settings.py
@@ -7,13 +7,12 @@ COOKIES_ENABLED = False
 TELNETCONSOLE_ENABLED = False
 LOG_LEVEL = 'ERROR'
 # 访问完一个页面再访问下一个时需要等待的时间，默认为10秒
-DOWNLOAD_DELAY = 0.6
+DOWNLOAD_DELAY = 0.5
 DEFAULT_REQUEST_HEADERS = {
     'Accept':
     'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
-    'cookie': 'SUB=_2A25PNbBfDeRhGeVH7lsX8yjNyDyIHXVs2dAXrDV6PUJbktCOLRSikW1NT1UZ-0tpdwbAB-CAGA7Bpd-S2SYuoxj5; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W55RbeT-cpPMK4Jx0nTOOg85NHD95Q01K-4SoeceKe7Ws4Dqcj_i--ci-24iK.Ri--ciK.Ri-2fi--fiKy2iKnpi--fiKnfi-82i--fiK.ci-zf; SSOLoginState=1647427601; _T_WM=70431602826; WEIBOCN_FROM=1110006030; MLOGIN=1; M_WEIBOCN_PARAMS=uicode%3D20000174; XSRF-TOKEN=1d0c0f'
-}
+    'cookie': 'SCF=AhIe3SiFIijIiJGWKMAISfWRhgkSEJfd-OZIDKK5-ES7XPi76ElCoKR0Juy28iq3puoT5vgCd5gxe82zSirkHgc.; SUB=_2A25PFUNvDeRhGeVH7lsX8yjNyDyIHXVs9m0nrDV6PUJbktCOLXLDkW1NT1UZ-0FaGdsSPG_1Bv-GsKFsvT1Ianrm; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W55RbeT-cpPMK4Jx0nTOOg85NHD95Q01K-4SoeceKe7Ws4Dqcj_i--ci-24iK.Ri--ciK.Ri-2fi--fiKy2iKnpi--fiKnfi-82i--fiK.ci-zf; SSOLoginState=1645294399; _T_WM=15419093921; WEIBOCN_FROM=1110006030; MLOGIN=1; XSRF-TOKEN=a3054b; M_WEIBOCN_PARAMS=luicode%3D20000174%26uicode%3D20000174'}
 ITEM_PIPELINES = {
     'weibo.pipelines.DuplicatesPipeline': 300,
     'weibo.pipelines.CsvPipeline': 301,
@@ -24,7 +23,7 @@ ITEM_PIPELINES = {
 }
 # 要搜索的关键词列表，可写多个, 值可以是由关键词或话题组成的列表，也可以是包含关键词的txt文件路径，
 # 如'keyword_list.txt'，txt文件中每个关键词占一行
-KEYWORD_LIST = ['小曲']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
+KEYWORD_LIST = ['#甄嬛#']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
 # 要搜索的微博类型，0代表搜索全部微博，1代表搜索全部原创微博，2代表热门微博，3代表关注人微博，4代表认证用户微博，5代表媒体微博，6代表观点微博
 WEIBO_TYPE = 0
 # 筛选结果微博中必需包含的内容，0代表不筛选，获取全部微博，1代表搜索包含图片的微博，2代表包含视频的微博，3代表包含音乐的微博，4代表包含短链接的微博
@@ -33,9 +32,9 @@ CONTAIN_TYPE = 0
 # 具体支持的地名见region.py文件，注意只支持省或直辖市的名字，省下面的市名及直辖市下面的区县名不支持，不筛选请用”全部“
 REGION = ['全部']
 # 搜索的起始日期，为yyyy-mm-dd形式，搜索结果包含该日期
-START_DATE = '2016-04-18'
+START_DATE = '2011-11-17'
 # 搜索的终止日期，为yyyy-mm-dd形式，搜索结果包含该日期
-END_DATE = '2016-08-30' 
+END_DATE = '2012-03-12'
 # 进一步细分搜索的阈值，若结果页数大于等于该值，则认为结果没有完全展示，细分搜索条件重新搜索以获取更多微博。数值越大速度越快，也越有可能漏掉微博；数值越小速度越慢，获取的微博就越多。
 # 建议数值大小设置在40到50之间。
 FURTHER_THRESHOLD = 46
--- weibo/spiders/search.py
+++ weibo/spiders/search.py
@@ -109,18 +109,12 @@ class SearchSpider(scrapy.Spider):
                 yield weibo
             next_url = response.xpath(
                 '//a[@class="next"]/@href').extract_first()
-            pagenum = 1
             if next_url:
                 next_url = self.base_url + next_url
-                pagenum += 1
                 yield scrapy.Request(url=next_url,
                                      callback=self.parse_page,
-                                     meta={
-                                         'keyword': keyword,
-                                         'pagenum': pagenum
-                                     })
+                                     meta={'keyword': keyword})
         else:
-            print('Split into days')
             start_date = datetime.strptime(self.start_date, '%Y-%m-%d')
             end_date = datetime.strptime(self.end_date, '%Y-%m-%d')
             while start_date <= end_date:
@@ -159,19 +153,12 @@ class SearchSpider(scrapy.Spider):
                 yield weibo
             next_url = response.xpath(
                 '//a[@class="next"]/@href').extract_first()
-            pagenum = 1
             if next_url:
                 next_url = self.base_url + next_url
-                pagenum += 1
                 yield scrapy.Request(url=next_url,
                                      callback=self.parse_page,
-                                     meta={
-                                         'keyword': keyword,
-                                         'date': date,
-                                         'pagenum': pagenum
-                                     })
+                                     meta={'keyword': keyword})
         else:
-            print('Split {} into hours'.format(date))
             start_date_str = date + '-0'
             start_date = datetime.strptime(start_date_str, '%Y-%m-%d-%H')
             for i in range(1, 25):
@@ -206,43 +193,83 @@ class SearchSpider(scrapy.Spider):
         page_count = len(response.xpath('//ul[@class="s-scroll"]/li'))
         if is_empty:
             print('当前页面搜索结果为空')
-        else:
+        elif page_count < self.further_threshold:
             # 解析当前页面
             for weibo in self.parse_weibo(response):
                 self.check_environment()
                 yield weibo
             next_url = response.xpath(
                 '//a[@class="next"]/@href').extract_first()
-            pagenum = 1
             if next_url:
                 next_url = self.base_url + next_url
-                pagenum += 1
                 yield scrapy.Request(url=next_url,
+                                     callback=self.parse_page,
+                                     meta={'keyword': keyword})
+        else:
+            for region in self.regions.values():
+                url = ('https://s.weibo.com/weibo?q={}&region=custom:{}:1000'
+                       ).format(keyword, region['code'])
+                url += self.weibo_type
+                url += self.contain_type
+                url += '&timescope=custom:{}:{}&page=1'.format(
+                    start_time, end_time)
+                # 获取一小时一个省的搜索结果
+                yield scrapy.Request(url=url,
+                                     callback=self.parse_by_hour_province,
+                                     meta={
+                                         'keyword': keyword,
+                                         'start_time': start_time,
+                                         'end_time': end_time,
+                                         'province': region
+                                     })
+
+    def parse_by_hour_province(self, response):
+        """以小时和直辖市/省为单位筛选"""
+        keyword = response.meta.get('keyword')
+        is_empty = response.xpath(
+            '//div[@class="card card-no-result s-pt20b40"]')
+        start_time = response.meta.get('start_time')
+        end_time = response.meta.get('end_time')
+        province = response.meta.get('province')
+        page_count = len(response.xpath('//ul[@class="s-scroll"]/li'))
+        if is_empty:
+            print('当前页面搜索结果为空')
+        elif page_count < self.further_threshold:
+            # 解析当前页面
+            for weibo in self.parse_weibo(response):
+                self.check_environment()
+                yield weibo
+            next_url = response.xpath(
+                '//a[@class="next"]/@href').extract_first()
+            if next_url:
+                next_url = self.base_url + next_url
+                yield scrapy.Request(url=next_url,
+                                     callback=self.parse_page,
+                                     meta={'keyword': keyword})
+        else:
+            for city in province['city'].values():
+                url = ('https://s.weibo.com/weibo?q={}&region=custom:{}:{}'
+                       ).format(keyword, province['code'], city)
+                url += self.weibo_type
+                url += self.contain_type
+                url += '&timescope=custom:{}:{}&page=1'.format(
+                    start_time, end_time)
+                # 获取一小时一个城市的搜索结果
+                yield scrapy.Request(url=url,
                                      callback=self.parse_page,
                                      meta={
                                          'keyword': keyword,
-                                         'date': start_time,
-                                         'pagenum': pagenum
+                                         'start_time': start_time,
+                                         'end_time': end_time,
+                                         'province': province,
+                                         'city': city
                                      })
 
     def parse_page(self, response):
         """解析一页搜索结果的信息"""
         keyword = response.meta.get('keyword')
-        meta = response.meta
         is_empty = response.xpath(
             '//div[@class="card card-no-result s-pt20b40"]')
-
-        new_meta = meta
-        if not 'pagenum' in new_meta:
-            new_meta['pagenum'] = 1
-        output = 'date={}'.format(meta['date'])
-        if 'province_name' in meta:
-            output += ', province={}'.format(meta['province_name'])
-        if 'city_name' in meta:
-            output += ', city={}'.format(meta['city_name'])
-        output += ', page={}'.format(new_meta['pagenum'])
-        print(output)
-
         if is_empty:
             print('当前页面搜索结果为空')
         else:
@@ -251,12 +278,11 @@ class SearchSpider(scrapy.Spider):
                 yield weibo
             next_url = response.xpath(
                 '//a[@class="next"]/@href').extract_first()
-            new_meta['pagenum'] += 1
             if next_url:
                 next_url = self.base_url + next_url
                 yield scrapy.Request(url=next_url,
                                      callback=self.parse_page,
-                                     meta=new_meta)
+                                     meta={'keyword': keyword})
 
     def get_article_url(self, selector):
         """获取微博头条文章url"""
@@ -490,4 +516,5 @@ class SearchSpider(scrapy.Spider):
                     retweet['retweet_id'] = ''
                     yield {'weibo': retweet, 'keyword': keyword}
                     weibo['retweet_id'] = retweet['id']
+                print(weibo)
                 yield {'weibo': weibo, 'keyword': keyword}
