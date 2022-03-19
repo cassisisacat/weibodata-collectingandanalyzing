688dcba975a27e58d55d32922469fd59c70dc8fc
Cassis Martin <71492383+cassisisacat@users.noreply.github.com>
2022-03-19 23:31:42 +0800

    Update train.py

--- step4_sentiments/train_model/train.py
+++ step4_sentiments/train_model/train.py
@@ -7,5 +7,5 @@ Created on Thu Jan  4 09:59:46 2018
 """
 from snownlp import sentiment
 
-sentiment.train('C:\\Users\\moban\\Documents\\Cassis\\weibo-search\\step4_sentiments\\train_model\\negative_dict.txt', 'C:\\Users\\moban\\Documents\\Cassis\\weibo-search\\step4_sentiments\\train_model\\positive_dict.txt')
-sentiment.save('C:\\Users\\moban\\Documents\\Cassis\\weibo-search\\step4_sentiments\\train_model\\sentiment.marshal')
+sentiment.train('路径\negative_dict.txt', '路径\positive_dict.txt')
+sentiment.save('路径\sentiment.marshal')
